d6261bdbee655032ff1b12476333faa16c9e96e6
evabothra <78145030+evabothra@users.noreply.github.com>
2021-06-09 17:04:45 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,15 @@
+class Slingshot{
+ constructor(bodyA, pointB){
+     var options = 
+     { bodyA: bodyA, 
+        pointB: pointB, 
+        stiffness: 0.04, 
+        length: 1 
+    } 
+    this.sling = Constraint.create(options); 
+    this.pointB=pointB; 
+    World.add(world, this.sling);
+
+
+        }
+}
